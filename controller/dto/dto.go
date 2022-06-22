661aab495550d0521e28eb98e9c9f40ea08c4f82
0xpeIpeI <hokumaru12@gmail.com>
2022-06-22 17:57:15 +0900

    dtoとentityの追加

--- controller/dto/dto.go
+++ controller/dto/dto.go
@@ -1,1 +1,16 @@
 package dto
+
+type TodoResponse struct {
+	ID      int    `jsong:"id"`
+	Title   string `jsong:"title"`
+	Content string `jsong:"content"`
+}
+
+type TodoRequest struct {
+	Title   string `jsong:"title"`
+	Content string `jsong:"content"`
+}
+
+type TodoResponses struct {
+	Todos []TodoResponse `json:"todos"`
+}
