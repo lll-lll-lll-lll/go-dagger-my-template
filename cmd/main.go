47187c3843eaecb572481f2cd595d148d40f7a98
0xpeIpeI <hokumaru12@gmail.com>
2022-06-22 16:35:19 +0900

    fileの移動

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+	r.HandleFunc("/", sampleHandler1).Methods("GET")
+}
+
+func sampleHandler1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello sample")
+}
