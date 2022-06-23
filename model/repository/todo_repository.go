8fbca5b5b670b712a5789c651fecc0b41d7b6eac
0xpeIpeI <hokumaru12@gmail.com>
2022-06-23 10:20:51 +0900

    repositoryのcrud処理追加

--- model/repository/todo_repository.go
+++ model/repository/todo_repository.go
@@ -1,7 +1,13 @@
 package repository
 
-import "github.com/go-dagger/model/entity"
+import (
+	"log"
 
+	"github.com/go-dagger/model/entity"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// インターフェース
 type TodoRepository interface {
 	GetTodos() (todos []entity.TodoEntity, err error)
 	InsertTodo(todo entity.TodoEntity) (id int, err error)
@@ -9,6 +15,7 @@ type TodoRepository interface {
 	DeleteTodo(id int) (err error)
 }
 
+// 具体的な実装. TodoRepository interfaceを実装
 type todoRepository struct {
 }
 
@@ -17,10 +24,46 @@ func NewTodoRepository() TodoRepository {
 	return &todoRepository{}
 }
 
-func (tr *todoRepository) GetTodos() (todos []entity.TodoEntity, err error) {}
+func (tr *todoRepository) GetTodos() (todos []entity.TodoEntity, err error) {
+	todos = []entity.TodoEntity{}
+	rows, err := Db.
+		Query("SELECT id, title, content FROM todo ORDER BY id DESC")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	for rows.Next() {
+		todo := entity.TodoEntity{}
+		err = rows.Scan(&todo.Id, &todo.Title, &todo.Content)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		todos = append(todos, todo)
+	}
+	return
+}
 
-func (tr *todoRepository) InsertTodo(todo entity.TodoEntity) (id int, err error) {}
+func (tr *todoRepository) InsertTodo(todo entity.TodoEntity) (id int, err error) {
+	// 引数で受け取ったEntityの値を元にDBに追加
+	_, err = Db.Exec("INSERT INTO todo (title, content) VALUES (?, ?)", todo.Title, todo.Content)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	// created_atが最新のTODOのIDを返却
+	err = Db.QueryRow("SELECT id FROM todo ORDER BY id DESC LIMIT 1").Scan(&id)
+	return
+}
 
-func (tr *todoRepository) UpdateTodo(todo entity.TodoEntity) (err error) {}
+func (tr *todoRepository) UpdateTodo(todo entity.TodoEntity) (err error) {
+	// 引数で受け取ったEntityの値を元にDBを更新
+	_, err = Db.Exec("UPDATE todo SET title = ?, content = ? WHERE id = ?", todo.Title, todo.Content, todo.Id)
+	return
+}
 
-func (tr *todoRepository) DeleteTodo(id int) (err error) {}
+func (tr *todoRepository) DeleteTodo(id int) (err error) {
+	// 引数で受け取ったIDの値を元にDBから削除
+	_, err = Db.Exec("DELETE FROM todo WHERE id = ?", id)
+	return
+}
